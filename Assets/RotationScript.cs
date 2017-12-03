1acf300ad47bd10be2d0b70eaa4ad837b7eff038
Giroux Guillaume <guillaume.giroux@ynov.com>
2017-12-03 16:12:46 +0100

    ajout fonctionnalités

---
+++ Assets/RotationScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationScript : MonoBehaviour {
+    public int Speed;
+
+    // Use this for initialization
+    void Start () {
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float f = Time.deltaTime * Speed;
+        var platform = GetComponent<Transform>();
+        platform.Rotate(new Vector3(0, f, 0));
+    }
+}
---
+++ Assets/TrigerDoor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrigerDoor : MonoBehaviour {
+    public float timedoor;
+    public bool opening;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (opening == true)
+        {
+            timedoor += Time.deltaTime;
+            if (timedoor < 4)
+            {
+                transform.Translate(Vector3.up * Time.deltaTime * 8, Space.World);
+            }
+            else
+            {
+                timedoor = 0;
+                opening = false;
+            }
+        }
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "FirstPersonCharacter")
+        {
+            opening = true; 
+        }
+    }
+}
